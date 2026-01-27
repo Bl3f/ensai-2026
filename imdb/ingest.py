d6f0f59c488611891f0407956441c8b639972d7c
Christophe Blefari <christophe.blefari@gmail.com>
2026-01-27 12:16:38 +0100

    Add data to BigQuery

--- imdb/ingest.py
+++ imdb/ingest.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 import click
 from google.cloud import storage
+from google.cloud import bigquery
+
 
 BASE_URL = "https://datasets.imdbws.com/"
 RAW_DATA_FOLDER = "data"
@@ -41,18 +43,71 @@ def upload_to_gcs(filename, destination):
     print("\tDone.")
 
 
+def ensure_dataset_exists(dataset_id):
+    client = bigquery.Client(project=PROJECT_ID)
+    dataset_ref = f"{PROJECT_ID}.{dataset_id}"
+
+    try:
+        client.get_dataset(dataset_ref)
+    except Exception:
+        dataset = bigquery.Dataset(dataset_ref)
+        dataset.location = "EU"  # or "US" depending on your preference
+        client.create_dataset(dataset)
+        print(f"\tCreated dataset {dataset_ref}")
+
+
+def create_bigquery_table_from_gcs(gcs_uri, dataset_id, table_id):
+    """
+    Create a BigQuery table from a Parquet file in GCS.
+
+    Args:
+        gcs_uri: Full GCS path, e.g., "gs://bucket-name/path/to/file.parquet"
+        dataset_id: BigQuery dataset ID
+        table_id: BigQuery table ID
+    """
+    print(f"\tCreating BigQuery table {dataset_id}.{table_id} from {gcs_uri}...")
+
+    client = bigquery.Client(project=PROJECT_ID)
+
+    table_ref = f"{PROJECT_ID}.{dataset_id}.{table_id}"
+
+    ensure_dataset_exists(dataset_id)
+
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.PARQUET,
+        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,  # Overwrite if exists
+    )
+
+    load_job = client.load_table_from_uri(
+        gcs_uri,
+        table_ref,
+        job_config=job_config,
+    )
+
+    load_job.result()  # Wait for the job to complete
+
+    table = client.get_table(table_ref)
+    print(f"\tLoaded {table.num_rows} rows to {table_ref}.")
+
+
 def ingest(table, force_download=False):
     filename = f"{table}.tsv.gz"
     raw_filename = os.path.join(RAW_DATA_FOLDER, filename)
     parquet_filename = os.path.join(
         PARQUET_DATA_FOLDER, filename.replace(".tsv.gz", ".parquet")
     )
+    gcs_file_uri = f"imdb/{table}/{table}.parquet"
 
     if force_download:
         get_data(os.path.join(BASE_URL, filename), raw_filename)
+        convert_to_parquet(raw_filename, parquet_filename)
+        upload_to_gcs(parquet_filename, gcs_file_uri)
 
-    convert_to_parquet(raw_filename, parquet_filename)
-    upload_to_gcs(parquet_filename, f"imdb/{table}/{table}.parquet")
+    create_bigquery_table_from_gcs(
+        os.path.join("gs://", BUCKET_NAME, gcs_file_uri),
+        "bronze_christophe",
+        table.replace(".", "_"),
+    )
 
 
 @click.command()
